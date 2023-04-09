e66de0545c84098d44b79ed766e889b5973b73e7
Bryt19 <airxqhobarbryt@gmail.com>
2023-04-09 07:41:05 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned integer num_bits = sizeof(unsigned long int) * 8;
+	unsigned int num_bits = sizeof(unsigned long int) * 8;
 
 	if (index >= num_bits)
 		return (-1);
